2d69bde8c3e78fffa31d60bd71d22e8353d66a33
Muhammad Salman <salmanfateh12@gmail.com>
2026-07-08 15:27:55 +0500

    variable and data types

---
+++ variable.py
@@ -0,0 +1,38 @@
+print ("Hello word ")
+n="Salman" 
+a=87
+g=3.0
+
+#if (a>g):
+#print("five is graeter then two ")
+#print ("five is graeter then two")  
+#this is  the comment 
+#first 
+#last 
+#comment 
+x=str(3)
+y =int (3)
+z=float (3)
+print(x)
+print(y)
+print (z) 
+print (type (x))
+print (type (y))
+print (type (z))  
+a= 5
+a ="Salman "
+print (a)
+print (a) 
+
+x,y,z="orange ","banana ","cherry"
+print(x,y,z)
+x=y=z= "salman"
+print (x,y,z)
+fruits=["apple ","banana ","cherrry"]
+x,y,z=fruits
+print (x,y,z)  
+
+
+y=3
+z=3
+print (y**3)
